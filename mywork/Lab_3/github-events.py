9bc5f38efdc5126936138780022a9ea5b9d9d853
Daughter-of-most-high53 <evelynxanixa.heartc@gmail.com>
2025-04-15 00:44:08 -0400

    Removed nested Git repo and added as regular folder

--- mywork/Lab_3/github-events.py
+++ mywork/Lab_3/github-events.py
@@ -5,14 +5,5 @@ import json
 import requests
 
 GHUSER = os.getenv('Daughter-of-the-mosthigh53')
-
 url = 'https://github.com/Daughter-of-most-high53/DS-2002-Spr25'.format(GHUSER)
-
-r = json.loads(requests.get(url).text)
-
-for x in r[:5]:
-  event = x['type'] + ' :: ' + x['repo']['name']
-  print(event)
-
-
 
