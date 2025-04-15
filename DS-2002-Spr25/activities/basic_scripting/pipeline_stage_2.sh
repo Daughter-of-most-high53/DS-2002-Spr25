c7a938d543b1a6746458af6aee29f58ac157d9c3
Daughter-of-most-high53 <evelynxanixa.heartc@gmail.com>
2025-04-15 01:21:59 -0400

    Removed accidentally cloned nested DS-2002-Spr25 folder

--- DS-2002-Spr25/activities/basic_scripting/pipeline_stage_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while IFS= read -r line; do
-  value=$(echo "$line" | cut -d',' -f2)
-  result=$((value * 2))
-  echo "$line,$result"
-done
--- DS-2002-Spr25/activities/basic_scripting/process_file_unattended.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while IFS= read -r line; do
-  value=$(echo "$line" | cut -d',' -f2) # Extracting the second field (example)
-  if [[ "$value" -gt 10 ]]; then # Example logic
-    echo "$line"
-  fi
-done
